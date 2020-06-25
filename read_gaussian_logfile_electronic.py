bfb958de447b95bc8a8fb0854daac0e9ab50f12a
JacksonBurns <JacksonBurns@users.noreply.github.com>
2020-06-25 15:16:59 -0400

    code simplification

--- read_gaussian_logfile_electronic.py
+++ read_gaussian_logfile_electronic.py
@@ -24,104 +24,68 @@ import os.path
 from argparse import ArgumentParser
 import re
 
-from helper_files import IterateSubdirectories
+# from helper_files import IterateSubdirectories
 
+def get_descriptor(filepath,regex,name):
+    """
+    Helper function to build requests 
+
+    :param filepath: path to logfile
+    :param regex: Regular expression (as a raw string) used to locate the descriptor
+    :param name: String containing name of parameters in case not found
+    :return: float zero point descriptor if found. 
+        False if nothing found
+    """
+    with open(filepath, "r") as file:
+        file_data = file.readlines()
+    pattern = re.compile(regex)
+    for line in file_data:
+        m = pattern.search(line)
+        if m:
+            # match found
+            return float(m[1])
+    print(name+"not found!")
+    return False
 
 def get_zpe(filepath):
     """
-
-    :param filepath: log file path
     :return: float zero point correction (Hartrees/atom) if found. 
         False if nothing found
     """
-    with open(filepath, "r") as fp:
-        file_data = fp.readlines()
-    zpe_pattern = re.compile(r'Zero-point correction=[ ]*(-?\d+\.?\d+)')
-    for line in file_data:
-        m = zpe_pattern.search(line)
-        if m:
-            # match found
-            zpe = m[1]
-            return float(zpe)
-    print('zero-point correction not found')
-    return False
+    return get_descriptor(filepath,
+        r'Zero-point correction=[ ]*(-?\d+\.?\d+)',
+        "Zero point correction")
 
 
 def get_electronic_zpe(filepath):
     """
-
-    :param filepath: log file path
-    :return: float elec_zpe (Hartrees/atom) if found. False if nothing found
+    :return: float elec_zpe (Hartrees/atom) if found. 
+        False if nothing found
     """
-    with open(filepath, "r") as fp:
-        file_data = fp.readlines()
-    elec_zpe_pattern = re.compile(
-        r'Sum of electronic and zero-point Energies=[ ]*(-?\d+\.?\d+)')
-    for line in file_data:
-        m = elec_zpe_pattern.search(line)
-        if m:
-            # pattern matches
-            elec_zpe = m[1]
-            return float(elec_zpe)
-    print('no electronic energy found')
-    return False
+    return get_descriptor(filepath,
+        r'Sum of electronic and zero-point Energies=[ ]*(-?\d+\.?\d+)',
+        "electronic energy")
 
 
 def get_free_energy(filepath):
-    """Get the thermal free energy from a Gaussian logfile.
-
-    Parameters
-    ----------
-    filepath : str
-        Complete path of the Gaussian logfile.
-
-    Returns
-    -------
-    float
-       free energy in Hartree/ atom if found.
+    """
+    :return: float free energy in Hartree/ atom if found.
        Prints ('No free energy found') and returns False if none found.
 
     """
-    with open(filepath, "r") as fp:
-        file_data = fp.readlines()
-    free_energy_pattern = re.compile(
-        r'Sum of electronic and thermal Free Energies=[ ]*(-?\d+\.?\d+)')
-    for line in file_data:
-        m = free_energy_pattern.search(line)
-        if m:
-            # pattern matches
-            free_energy = m[1]
-            return float(free_energy)
-    print('no free energy found')
-    return False
+    return get_descriptor(filepath,
+        r'Sum of electronic and thermal Free Energies=[ ]*(-?\d+\.?\d+)',
+        "free energy")
 
 
 def get_enthalpy(filepath):
-    """Get the thermal enthalpy from a Gaussian logfile.
-
-    Parameters
-    ----------
-    filepath : str
-        Complete path of the Gaussian logfile.
-
-    Returns
-    -------
-    float
-       Enthalpy in Hartree/ atom if found.
-       Prints ('No enthalpy found') and returns False if none found.
+    """
+    :return: float Enthalpy in Hartree/ atom if found.
+        Prints ('No enthalpy found') and returns False if none found.
 
     """
-    with open(filepath, "r") as fp:
-        file_data = fp.readlines()
-    enthalpy_pattern = re.compile(
-        r'Sum of electronic and thermal Enthalpies=[ ]*(-?\d+\.?\d+)')
-    for line in file_data:
-        m = enthalpy_pattern.search(line)
-        if m:
-            # pattern matches
-            enthalpy = m[1]
-            return float(enthalpy)
-    print('no enthalpy found')
-    return False
+    return get_descriptor(filepath,
+        r'Sum of electronic and thermal Enthalpies=[ ]*(-?\d+\.?\d+)',
+        "enthalpy")
 
 # 'D:\Research\Error_DFT\Data_fusion\Jobs' -qf 'D:\Research\Error_DFT\Data_fusion\QM9\xyz_choose_2000_for_gaussian'
